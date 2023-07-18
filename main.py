6063c3d288cdb74b2e9a8fe59990e84fbf2207ab
Yuliya <fealka79@gmail.com>
2023-07-18 16:58:40 +0300

    Блюдо2 с ошибкой

--- main.py
+++ main.py
@@ -17,7 +17,8 @@
 # # Button(root, image=pict, command=lambda: print('click')).pack()
 # MyButton('image.jpg', command=lambda: print('click')).pack()
 # root.mainloop()
-
+import asyncio
+import os
 # https://quotes.toscrape.com/
 # Парсинг
 
@@ -185,11 +186,12 @@
 
 # Асинхронное программирование
 
-
+import os
 import time
 from datetime import datetime
+import asyncio
 
-def dish(num, prepare, wait):
+async def dish(num, prepare, wait):
     """
 
     num: номер блюда по порядку
@@ -200,13 +202,23 @@ def dish(num, prepare, wait):
     print(f'{datetime.now().strftime("%H:%M:%S")} - подготовка к приготовлению блюда {num} - {prepare} мин')
     time.sleep(prepare)
     print(f'Начало приготовления {num} - {datetime.now().strftime("%H:%M:%S")}. Ожидание блюда {num} : {wait} мин')
-    time.sleep(prepare)
+    time.sleep(wait)
+    await asyncio.sleep(wait)
     print(f'В {datetime.now().strftime("%H:%M:%S")} блюдо {num} готово.')
 
-t0 = time.time()  # время начала работы
-dish(1, 2, 3)
-dish(2, 5, 10)
-dish(3, 3, 5)
-delta = int(time.time() - t0)  # затраченное время
-print(f'В {datetime.now().strftime("%H:%M:%S")} мы закончили')
-print(f'Затрачено время - {delta}')
+async def main():
+    tasks = [
+        asyncio.create_task(dish(1, 2, 3)),
+        asyncio.create_task(dish(2, 5, 10)),
+        asyncio.create_task(dish(3, 3, 5))
+    ]
+    await asyncio.gather(*tasks)
+
+if  __name__ == '__main__':
+    t0 = time.time()  # время начала работы
+    if os.name == 'nt':
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+    asyncio.run(main())
+    delta = int(time.time() - t0)  # затраченное время
+    print(f'В {datetime.now().strftime("%H:%M:%S")} мы закончили')
+    print(f'Затрачено время - {delta}')
