97cc86252f04b8080f1b1836dc870727d4d19769
Yuliya <fealka79@gmail.com>
2023-07-19 10:06:13 +0300

    Блюдо2

--- main.py
+++ main.py
@@ -192,19 +192,18 @@ from datetime import datetime
 import asyncio
 
 async def dish(num, prepare, wait):
-    """
 
+    """
     num: номер блюда по порядку
     prepare: время на подготовку
     wait: ожидание готовности
     """
 
-    print(f'{datetime.now().strftime("%H:%M:%S")} - подготовка к приготовлению блюда {num} - {prepare} мин')
+    print(f'{datetime.now().strftime("%H:%S")} - подготовка к приготовлению блюда {num} - {prepare} мин.')
     time.sleep(prepare)
-    print(f'Начало приготовления {num} - {datetime.now().strftime("%H:%M:%S")}. Ожидание блюда {num} : {wait} мин')
-    time.sleep(wait)
+    print(f'Начало приготовления блюда {num} - {datetime.now().strftime("%H:%S")}. Ожидание блюда {num} {wait} мин.')
     await asyncio.sleep(wait)
-    print(f'В {datetime.now().strftime("%H:%M:%S")} блюдо {num} готово.')
+    print(f'В {datetime.now().strftime("%H:%S")}. блюдо {num} готово.')
 
 async def main():
     tasks = [
@@ -214,11 +213,11 @@ async def main():
     ]
     await asyncio.gather(*tasks)
 
-if  __name__ == '__main__':
-    t0 = time.time()  # время начала работы
+if __name__ == '__main__':
+    t0 = time.time()  # время начало работы
     if os.name == 'nt':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(main())
+    asyncio.run(main()) # запустили асинхронное приготовление
     delta = int(time.time() - t0)  # затраченное время
-    print(f'В {datetime.now().strftime("%H:%M:%S")} мы закончили')
-    print(f'Затрачено время - {delta}')
+    print(f'В {datetime.now().strftime("%H:%S")} мы закончили')
+    print(f'Затрачено времени - {delta}')
